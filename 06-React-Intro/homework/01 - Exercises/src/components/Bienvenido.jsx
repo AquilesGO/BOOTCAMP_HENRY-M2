77c70aaac6e9f7f3054a507200dd11a9f8c27a16
Aquileo <agutov@gmail.com>
2022-11-01 11:07:27 -0500

    Tarea React intro

--- 06-React-Intro/homework/01 - Exercises/src/components/Bienvenido.jsx
+++ 06-React-Intro/homework/01 - Exercises/src/components/Bienvenido.jsx
@@ -1,13 +1,31 @@
 import React from "react";
+import Botones from "./Botones.jsx";
 
-const studentName = "";
+const studentName = "Aquileo";
 const techSkills = ["Html", "Css", "JavaScript", "React", "Redux"];
 const alerts = { m1: "Aprobado", m2: "En curso" };
 
 export default function Bienvenido() {
   // el código de tu componente acá
-  return;
-}
+    return(
+      <div>
+        <h1>soy Henry!</h1>
+        <h3>{studentName}</h3>
 
+        {/* Code review: */}
+        <ul>
+        {techSkills.map(skill => (
+            <li>{skill}</li>
+          ))}
+        </ul>
+        {/* <ul>
+          {techSkills.map((elemento, index) => {
+            return <li key={index}> {elemento}</li>;
+          })}
+        </ul> */}
+        <Botones alerts={alerts}/>
+      </div>
+    )
+}
 // Esto lo exportamos para los tests
 export { studentName, techSkills, alerts };
