7ce36092f7dab5fff01e2fce18d62aa93ad02b9d
Aquileo <agutov@gmail.com>
2022-11-09 11:03:39 -0500

    lo que faltaba en Forms para que corriera

--- 10-React-Forms/homework/01 - Exercises/src/components/Contact/Contact.jsx
+++ 10-React-Forms/homework/01 - Exercises/src/components/Contact/Contact.jsx
@@ -4,6 +4,7 @@ import "./Contact.modules.css";
 // eslint-disable-next-line
 const regexEmail = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i;
 
+
 export function validate(inputs) {
   let errors = {};
   if (!inputs.name) errors.name = "Se requiere un nombre";
